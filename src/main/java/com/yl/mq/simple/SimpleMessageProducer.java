ce7c58e08795e4dfc937ddf97b5586fe17181ff1
renbf <864378535@qq.com>
2018-12-26 22:11:30 +0800

    '发送确认'

--- src/main/java/com/yl/mq/simple/SimpleMessageProducer.java
+++ src/main/java/com/yl/mq/simple/SimpleMessageProducer.java
@@ -30,8 +30,11 @@ public class SimpleMessageProducer {
 			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 			// 消息内容
 			String message = "Hello World你好a!";
+			channel.confirmSelect();
 			channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-			log.info(" [x] Sent '" + message + "'");
+			if (channel.waitForConfirms()) {
+			    log.info(" [x] Sent '" + message + "'");
+			}
 			// 关闭通道和连接
 			channel.close();
 			connection.close();
--- src/main/java/com/yl/mq/work/Producer.java
+++ src/main/java/com/yl/mq/work/Producer.java
@@ -23,15 +23,21 @@ public class Producer {
 	        //3.声明(创建)队列
 	        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 	        //4.定义消息内容,发布多条消息
-	        for (int i = 0; i < 10; i++) {
-	            String message = "hello rabbitmq " + i;
-	            //5.发布消息
-	            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-	            System.out.println("[x] send message is '" + message + "'");
-	            //6.模拟发送消息延时,便于展示多个消费者竞争接受消息
-	            Thread.sleep(i * 10);
-	        }
-	        //7.关闭信道
+	        try {
+	        	channel.txSelect();
+				for (int i = 0; i < 10; i++) {
+					String message = "hello rabbitmq " + i;
+					//5.发布消息
+					channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+					System.out.println("[x] send message is '" + message + "'");
+					//6.模拟发送消息延时,便于展示多个消费者竞争接受消息
+					Thread.sleep(i * 10);
+				} 
+				channel.txCommit();
+			} catch (Exception e) {
+				channel.txRollback();
+			}
+			//7.关闭信道
 	        channel.close();
 	        //8.关闭连接
 	        connection.close();
